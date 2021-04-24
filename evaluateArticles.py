311c2852144917fe186dd52d16606b474257b677
francescoantici <francesco.antici98@gmail.com>
2021-04-24 13:03:35 +0200

    evaluation script split

---
+++ evaluateArticles.py
@@ -0,0 +1,59 @@
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Perceptron
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from evaluate_sentences_model import toLabels
+
+def create_articles_model(modelName = 'random-forest'):
+  modelSwitcher = {
+      "svm" : LinearSVC,
+      "logistic" : LogisticRegression,
+      "tree" : DecisionTreeClassifier,
+      "random-forest": RandomForestClassifier,
+      "naive-bayes" : MultinomialNB
+  }
+  return modelSwitcher[modelName]()
+
+def train_articles_model(model, Xtrain, ytrain):
+  try:
+    model.fit(X = Xtrain, y = ytrain)
+    print("Articles model trained successfully!")
+    return model
+  except Exception as e:
+    print("Error in training articles model: {}".format(e))
+    return False
+
+def evaluate_articles_model(model, Xtest, ytest):
+  y_pred = model.predict(Xtest)
+  ypred = list(map(lambda x: toLabels[x], y_pred))
+  ytest = list(map(lambda x: toLabels[x], ytest))
+  with open('results/reports_articles.txt', 'a') as f:
+    f.write(model+":\n")
+    f.write(classification_report(ytest, y_pred = ypred)+ "\n")
+
+def get_articles(split):
+  df = pd.read_csv("datasets/articles/articles{}.csv".format(split.capitalize()))
+  fonti = list(df['FONTE'].unique())
+  tags = {"OGG" : 0, "SOG" : 1}
+  X = df.drop(['ID_ARTICOLO', 'TAG_ARTICOLO'], axis = 1)
+  X['FONTE'] = X['FONTE'].map(lambda x: fonti.index(x))
+  scaler = MinMaxScaler()
+  scaler = scaler.fit(X)
+  X = scaler.transform(X)
+  y = np.array(list(map(lambda x: tags[x], df['TAG_ARTICOLO'].values)))
+  return X, y
+
+def main():
+    articlesModel = create_articles_model()
+    articlesXtrain, articlesytrain = get_articles(split='train')
+    articlesXTest, articlesytest = get_articles(split='test')
+    articlesModel = train_articles_model(articlesModel, articlesXtrain, articlesytrain)
+    evaluate_articles_model(articlesModel, articlesXTest, articlesytest)
+
+main()
--- evaluateSentences.py
+++ evaluateSentences.py
@@ -1,15 +1,7 @@
 import tensorflow_text as text
 import tensorflow as tf
 import tensorflow_hub as hub
-from sklearn.svm import LinearSVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import Perceptron
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
 def create_sentences_model():
@@ -24,16 +16,6 @@ def create_sentences_model():
   model.compile(optimizer = tf.optimizers.Adam(3e-5), loss = 'binary_crossentropy', metrics = ['accuracy'])
   
   return model
-
-def create_articles_model(modelName = 'random-forest'):
-  modelSwitcher = {
-      "svm" : LinearSVC,
-      "logistic" : LogisticRegression,
-      "tree" : DecisionTreeClassifier,
-      "random-forest": RandomForestClassifier,
-      "naive-bayes" : MultinomialNB
-  }
-  return modelSwitcher[modelName]()
 
 def train_sentences_model(model, Xtrain, ytrain, validation_data = None, save_weights = True):
   try:
@@ -54,41 +36,11 @@ def evaluate_sentences_model(model, Xtest, ytest):
   with open('results/reports_sentences.txt', 'a') as f:
     f.write(classification_report(ytest, y_pred = ypred)+ "\n")
 
-def train_articles_model(model, Xtrain, ytrain):
-  try:
-    model.fit(X = Xtrain, y = ytrain)
-    print("Articles model trained successfully!")
-    return model
-  except Exception as e:
-    print("Error in training articles model: {}".format(e))
-    return False
-
-def evaluate_articles_model(model, Xtest, ytest):
-  y_pred = model.predict(Xtest)
-  ypred = list(map(lambda x: toLabels[x], y_pred))
-  ytest = list(map(lambda x: toLabels[x], ytest))
-  with open('results/reports_articles.txt', 'a') as f:
-    f.write(model+":\n")
-    f.write(classification_report(ytest, y_pred = ypred)+ "\n")
-
-def get_articles(split):
-  df = pd.read_csv("datasets/articles/articles{}.csv".format(split.capitalize()))
-  fonti = list(df['FONTE'].unique())
-  tags = {"OGG" : 0, "SOG" : 1}
-  X = df.drop(['ID_ARTICOLO', 'TAG_ARTICOLO'], axis = 1)
-  X['FONTE'] = X['FONTE'].map(lambda x: fonti.index(x))
-  scaler = MinMaxScaler()
-  scaler = scaler.fit(X)
-  X = scaler.transform(X)
-  y = np.array(list(map(lambda x: tags[x], df['TAG_ARTICOLO'].values)))
-  return X, y
-
 def get_sentences(split):
   df = pd.read_csv("datasets/sentences/sentences{}.csv".format(split.capitalize()))
   tags = {"OGG" : 0, "SOG" : 1}
   return df['FRASE'].values, np.array(list(map(lambda x: tags[x], df['TAG_FRASE'].values)))
   
-
 def toLabels(data, subT = 0.5):
     ypred = []
     for pred in data:
@@ -100,20 +52,15 @@ def toLabels(data, subT = 0.5):
 
 def main():
   sentencesModel = create_sentences_model()
-  articlesModel = create_articles_model()
   sentencesXtrain, sentencesytrain = get_sentences(split = 'train')
   sentencesXval, sentencesyval = get_sentences(split = 'val')
   sentencesXtest, sentencesytest = get_sentences(split = 'test')
-  articlesXtrain, articlesytrain = get_articles(split='train')
-  articlesXTest, articlesytest = get_articles(split='test')
   try:
     sentencesModel.load_weights('weights/sentencesModelWeights.h5')
   except:
     sentencesModel = train_sentences_model(sentencesModel, sentencesXtrain, sentencesytrain, 
                                            validation_data = (sentencesXval, sentencesyval))
-  articlesModel = train_articles_model(articlesModel, articlesXtrain, articlesytrain)
   evaluate_sentences_model(sentencesModel, sentencesXtest, sentencesytest)
-  evaluate_articles_model(articlesModel, articlesXTest, articlesytest)
 
 main()
   
